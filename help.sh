ca03fd92883b5b273fae1af1adf3154a8af5910b
Jonathan Neal <jonathantneal@hotmail.com>
2013-10-25 00:18:55 -0700

    Short and sweet goodbye

--- help.sh
+++ help.sh
@@ -10,7 +10,7 @@ echo ""
 read -p "So, what would you like to install: " NAME
 
 if [ "$NAME" == "exit" ] || [ "$NAME" == "" ]; then
-	echo "Very well. Have a nice day!"
+	echo "Have a nice day!"
 else
 	bash <(curl wget.sh/${NAME/ /+} -s -L -A "")
 fi
