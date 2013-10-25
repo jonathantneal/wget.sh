5448542ba976db999fb9ccc535d6733ede8c31b7
Jonathan Neal <jonathantneal@hotmail.com>
2013-10-25 01:06:41 -0700

    [#1] Use HTTPS

--- json.sh
+++ json.sh
@@ -105,5 +105,5 @@ read -p "So, what would you like to install: " NAME
 if [ "$NAME" == "exit" ] || [ "$NAME" == "" ]; then
 	echo "Have a nice day!"
 else
-	bash <(curl wget.sh/${NAME/ /+} -s -L -A "")
+	bash <(curl https://wget.sh/${NAME/ /+} -s -A "")
 fi
